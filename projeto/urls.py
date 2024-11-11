f23143e77c34460c232bda1910ce52dccc11c5a8
Samuel Soares <samuelbji254@gmail.com>
2024-11-11 10:40:00 -0300

    Projeto alterado

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cardapio project.
+URL configuration for projeto project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
